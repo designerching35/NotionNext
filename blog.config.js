2c467fa9e478668319391f9a65d31be465c884de
designerching35 <136953087+designerching35@users.noreply.github.com>
2023-06-25 10:21:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -341,14 +341,4 @@ const BLOG = {
   VERSION: process.env.NEXT_PUBLIC_VERSION // 版本号
 }
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-L3CPNL047M"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-L3CPNL047M');
-</script>
-
 module.exports = BLOG
