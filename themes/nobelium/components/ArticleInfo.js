899a81c8378442a4a666eca754c202dd077f0799
designerching35 <136953087+designerching35@users.noreply.github.com>
2023-08-12 01:26:26 +0800

    Update ArticleInfo.js

--- themes/nobelium/components/ArticleInfo.js
+++ themes/nobelium/components/ArticleInfo.js
@@ -25,7 +25,7 @@ export const ArticleInfo = (props) => {
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src={`https://gravatar.com/avatar/happynews1015{emailHash}`}
+                  src={`https://gravatar.com/avatar/238416479{emailHash}`}
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
